08bd228ebe9763dbc5541ad9e52694c6d3088669
Isac Aguiar <isacaguiar@gmail.com>
2023-03-29 17:53:37 -0300

    Adjustment

--- src/main/java/br/com/isac/adapter/controller/response/TransactionStatusResponse.java
+++ src/main/java/br/com/isac/adapter/controller/response/TransactionStatusResponse.java
@@ -1,6 +1,6 @@
 package br.com.isac.adapter.controller.response;
 
-public class TransactionStatusResponse {
+public abstract class TransactionStatusResponse {
   public static final String OK = "OK";
   public static final String INSUFFICIENT_FUNDS = "SALDO_INSUFICIENTE";
   public static final String INVALID_PASSWORD = "SENHA_INVALIDA";
