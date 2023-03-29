609a2a6ff19a53d20ce0ac43dee7031332b3287e
Isac Aguiar <isacaguiar@gmail.com>
2023-03-29 17:46:42 -0300

    Adjustments

---
+++ src/test/java/br/com/isac/MiniAuthorizerApplicationTest.java
@@ -0,0 +1,19 @@
+package br.com.isac;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+class MiniAuthorizerApplicationTest {
+
+  @Test
+  public void testConfigure() {
+    String[] args = {""};
+    Runnable runnable = () -> MiniAuthorizerApplication.main(args);
+    assertNotNull(runnable);
+  }
+
+}
